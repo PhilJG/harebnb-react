28876758debcfcc02353624d92b26713f6525ef6
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-04-16 00:30:24 -0400

    Remove / from base url

--- src/_utils/fetch.js
+++ src/_utils/fetch.js
@@ -2,7 +2,20 @@ const getBaseUrl = (string) => {
     if(string.includes('localhost')){
         return 'http://localhost:4100'
     }
-    return 'https://harebnb-api.onrender.com/'
+    return 'https://harebnb-api.onrender.com'
 }
 
 export default getBaseUrl
+
+// const getBaseUrl = (string) => {
+//     let baseUrl;
+//     if(string.includes('localhost')){
+//         baseUrl = 'http://localhost:4100'
+//     } else {
+//         baseUrl = 'https://harebnb-api.onrender.com/'
+//     }
+//     // Remove trailing slash if it exists
+//     return baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl;
+// }
+
+// export default getBaseUrl
