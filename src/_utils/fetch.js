5425f9590a653f21892740482b4f415088abb292
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-07-26 10:52:26 -0700

    adjust api link url

--- src/_utils/fetch.js
+++ src/_utils/fetch.js
@@ -2,7 +2,7 @@ const getBaseUrl = (string) => {
   if (string.includes('localhost')) {
     return 'http://localhost:4100'
   }
-  return 'https://harebnb-react.onrender.com'
+  return 'https://harebnb-api.onrender.com'
 }
 
 export default getBaseUrl
